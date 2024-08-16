efdedf5a9a477d8ae5ca888ed4a83e746355d856
unknown <tientrancong75@gmail.com>
2024-08-16 15:40:53 +0700

    handle logic and ui profile

--- src/queries/useAccount.ts
+++
@@ -1,9 +0,0 @@
-import accountApiRequest from "@/apiRequests/account"
-import { useQuery } from "@tanstack/react-query"
-
-export const useAccountProfile = () => {
-    return useQuery({
-        queryKey: ["account", "profile"],
-        queryFn: accountApiRequest.me,
-    })
-}
---
+++ src/queries/useAccount.tsx
@@ -0,0 +1,15 @@
+import accountApiRequest from "@/apiRequests/account"
+import { useMutation, useQuery } from "@tanstack/react-query"
+
+export const useAccountMe = () => {
+    return useQuery({
+        queryKey: ["account", "profile"],
+        queryFn: accountApiRequest.me,
+    })
+}
+
+export const useUpdateMeMutation = () => {
+    return useMutation({
+        mutationFn: accountApiRequest.updateMe,
+    })
+}
